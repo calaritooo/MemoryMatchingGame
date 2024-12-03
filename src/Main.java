5df03ba782326351b23e60566bc95433fac8a570
Caleb Morgan <calaritooo@PC-di-Babbo.local>
2024-12-03 11:24:01 +0100

    Initial commit - Creation of repository

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Welcome to the game 'Memory Matching'!");
+
+    }
+
+    private static final int GRID_SIZE = 6;
+    private static final String THEME_1 = "Blocks";
+    private static final String THEME_2 = "Food";
+    private static String[][] grid = new String[GRID_SIZE][GRID_SIZE];
+}
